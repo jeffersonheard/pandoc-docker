b10f3dce3199772cd2f42ba230f7a376127c46de
Jefferson Heard <jeff@terrahub.io>
2014-09-17 12:21:15 -0400

    stuff

--- pandoc.sh
+++ pandoc.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo $INPUT_URL
+echo $INPUT_FORMAT
+echo $OUTPUT_FORMAT
+echo $RESPONSE_URL
+echo $ABORT_URL
+
 mkdir -p /tmp
 curl "$INPUT_URL" > /tmp/input.$INPUT_FORMAT
 pandoc -f $INPUT_FORMAT -t $OUTPUT_FORMAT -o /tmp/output.$OUTPUT_FORMAT /tmp/input.$INPUT_FORMAT
